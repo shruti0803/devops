04b54e85edae78ad9ae2361cbb84547a0f6b53ca
Shruti Chaurasia <shrutichaurasia39@gmail.com>
2024-11-04 14:29:33 +0530

    average

--- demo.c
+++ demo.c
@@ -1,5 +1,34 @@
-#include<stdio.h>
-int main(){
-    printf("i am c");
+#include <stdio.h> 
+
+
+float calculateAverage(int arr[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += arr[i]; 
+    }
+    return (float)sum / size; 
+}
+
+int main() {
+    int n;
+
+    
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int numbers[100]; 
+
+    printf("Enter %d numbers:\n", n);
+    for (int i = 0; i < n; i++) {
+        printf("Number %d: ", i + 1);
+        scanf("%d", &numbers[i]);
+    }
+
+   
+    float avg = calculateAverage(numbers, n);
+
+    
+    printf("The average is: %.2f\n", avg);
+
     return 0;
 }
